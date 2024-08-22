8a590c098affa98bca90519b01c4dc80d3aa8583
Daniel Rodriguez <administracion@consultoriaycapacitacionhseq.com>
2024-08-22 07:18:41 -0500

    add and delete companies for group

--- diagnostico_pesv/apps/corporate_group/models.py
+++ diagnostico_pesv/apps/corporate_group/models.py
@@ -7,11 +7,27 @@ from timestamps.models import SoftDeletes, Timestampable
 # Create your models here.
 class Corporate(SoftDeletes, Timestampable):
     name = models.CharField(max_length=200, null=True)
+    companies = models.ManyToManyField(
+        Company, through="Corporate_Company_Diagnosis", related_name="corporates"
+    )
+    diagnoses = models.ManyToManyField(
+        Diagnosis, through="Corporate_Company_Diagnosis", related_name="corporates"
+    )
 
 
 class Corporate_Company_Diagnosis(SoftDeletes, Timestampable):
-    company = models.ForeignKey(Company, on_delete=models.SET_NULL, null=True)
-    diagnosis = models.ForeignKey(Diagnosis, on_delete=models.SET_NULL, null=True)
+    company = models.ForeignKey(
+        Company,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="corporate_company_diagnoses",
+    )
+    diagnosis = models.ForeignKey(
+        Diagnosis,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="corporate_company_diagnoses",
+    )
     corporate = models.ForeignKey(
         Corporate,
         on_delete=models.SET_NULL,
