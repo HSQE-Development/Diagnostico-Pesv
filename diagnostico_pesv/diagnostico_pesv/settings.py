5521bb4cbeae04687f4645b4206e59bedd6a94ed
Daniel Rodriguez <dangelsfocus@gmail.com>
2024-10-16 11:06:58 -0500

    any

--- diagnostico_pesv/diagnostico_pesv/settings.py
+++ diagnostico_pesv/diagnostico_pesv/settings.py
@@ -235,5 +235,5 @@ CHANNEL_LAYERS = {
         "BACKEND": "channels.layers.InMemoryChannelLayer",
     },
 }
-CORS_ALLOWED_ORIGINS = ["pesvapp.consultoriaycapacitacionhseq.com"]
-ALLOWED_HOSTS = ["pesvapp.consultoriaycapacitacionhseq.com"]
+CORS_ALLOWED_ORIGINS = ["consultoriaycapacitacionhseq.com"]
+ALLOWED_HOSTS = ["consultoriaycapacitacionhseq.com"]
