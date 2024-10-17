98f494161b628570a94c50d6353690b0144ef389
Daniel Rodriguez <dangelsfocus@gmail.com>
2024-10-17 08:14:48 -0500

    updated allowed host

--- diagnostico_pesv/diagnostico_pesv/settings.py
+++ diagnostico_pesv/diagnostico_pesv/settings.py
@@ -69,7 +69,7 @@ INSTALLED_APPS = [
 
 
 CORS_ORIGIN_ALLOW_ALL = False
-ALLOWED_HOSTS = ["https://pesvapp.consultoriaycapacitacionhseq.com"]
+ALLOWED_HOSTS = ["apipesv.consultoriaycapacitacionhseq.com"]
 CORS_ORIGIN_WHITELIST = ["https://pesvapp.consultoriaycapacitacionhseq.com"]
 CORS_ALLOW_METHODS = list(default_methods)
 CORS_ALLOW_HEADERS = list(default_headers) + ["Authorization"]
