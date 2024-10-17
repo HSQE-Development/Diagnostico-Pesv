2a85df6b5728de840287c3c91d7033bf3060a7ce
Daniel Rodriguez <dangelsfocus@gmail.com>
2024-10-17 08:42:20 -0500

    update wsgi

--- diagnostico_pesv/diagnostico_pesv/wsgi.py
+++ diagnostico_pesv/diagnostico_pesv/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'diagnostico_pesv.settings')
+env = os.getenv("DEBUG", "False").lower() in ("true", "1")
+env_debug = "development" if env == True else "production"
+settings_module = f"diagnostico_pesv.settings.{env_debug}"
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
 application = get_wsgi_application()
