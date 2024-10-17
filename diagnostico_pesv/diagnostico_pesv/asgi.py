72a8d3cf4f41f69497e61a76f56c49201bf9a118
Daniel Rodriguez <dangelsfocus@gmail.com>
2024-10-17 08:48:20 -0500

    update to daphne

--- diagnostico_pesv/diagnostico_pesv/asgi.py
+++ diagnostico_pesv/diagnostico_pesv/asgi.py
@@ -13,7 +13,10 @@ from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 from . import routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "diagnostico_pesv.settings")
+env = os.getenv("DEBUG", "False").lower() in ("true", "1")
+env_debug = "development" if env == True else "production"
+settings_module = f"diagnostico_pesv.settings.{env_debug}"
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
 application = ProtocolTypeRouter(
     {
